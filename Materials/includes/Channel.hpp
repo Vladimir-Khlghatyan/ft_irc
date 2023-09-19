049eb009caa98a292d0610d960b16294010669ef
Vladimir Khlghatyan <vladimirkhlghatyan.m@gmail.com>
2023-09-19 20:09:19 +0400

    norming of Channel.hpp and Channel.cpp

--- Materials/includes/Channel.hpp
+++ Materials/includes/Channel.hpp
@@ -14,7 +14,6 @@ class Channel
         Channel(const std::string& channelName, const std::string& key);
         ~Channel();
 
-    public:
         void setKey(const std::string& key);
         void setTopic(const std::string& topic);
         void setAdmin(bool flagClosed);
@@ -25,25 +24,25 @@ class Channel
         std::string getChannelName(void);
         std::string getKey(void);
         std::string getTopic(void);
-        int getChannelLimit(void);
+        int         getChannelLimit(void);
+        Client*     getClientByNick(std::string nickname);
 
         void joinClient(Client* C);
         void kickClient(Client* C, const std::string& reason, bool flagClosed);
         bool isInChannel(Client* C);
-        void replyWho(Client* C, int mode);
         bool isAdmin(Client* C);
-        void sending(Client* C, const std::string& msg, const std::string& cmd);
-        void sendingForBot(Client* C, const std::string& msg, const std::string& cmd);
-        void nameReply(Client *C);
-        void part(Client *C, std::string reason);
-        bool emptyClients(void);
-        bool isOperator(Client *C);
+        bool isOperator(Client* C);
         bool isInviteOnly(void);
         bool topicModeIsOn(void);
-        Client *getClientByNick(std::string nickname);
-        void addOperator(Client *C);
-        void removeOperator(Client *C);
         bool channelIsFull(void);
+        void sending(Client* C, const std::string& msg, const std::string& cmd);
+        void sendingForBot(Client* C, const std::string& msg, const std::string& cmd);
+        void nameReply(Client* C);
+        void replyWho(Client* C, int mode);
+        void part(Client* C, std::string reason);
+        void addOperator(Client* C);
+        bool emptyClients(void);
+        void removeOperator(Client* C);
     
     private:
         std::string             _channelName;
@@ -56,5 +55,4 @@ class Channel
         std::vector<Client*>    _clients;
         std::vector<Client*>    _operators;
         Client*                 _admin;
-
 };
--- Materials/srcs/Channel.cpp
+++ Materials/srcs/Channel.cpp
@@ -1,6 +1,8 @@
 #include "Channel.hpp"
 #include "debugger.hpp"
 
+// ########################################################################################
+
 Channel::Channel(const std::string& channelName, const std::string& key) \
     : _channelName(channelName) \
     , _key(key)
@@ -10,155 +12,24 @@ Channel::Channel(const std::string& channelName, const std::string& key) \
     _limit = 0; // 0 limit means unlimit
 }
 
+// ########################################################################################
+
 Channel::~Channel()
 { }
 
-void Channel::setKey(const std::string& key)
-{
+// ########################################################################################
+
+void Channel::setKey(const std::string& key) {
     _key = key;
 }
 
-void Channel::setTopic(const std::string& topic)
-{
+// ########################################################################################
+
+void Channel::setTopic(const std::string& topic) {
     _topic = topic;
 }
 
-void Channel::setInviteOnly(bool mode)
-{
-    _inviteOnly = mode;
-}
-
-void Channel::setTopicMode(bool mode)
-{
-    _topicMode = mode;
-}
-
-void Channel::setChannelLimit(int limit)
-{
-    _limit = limit;
-}
-
-std::string Channel::getChannelName(void)
-{
-    return _channelName;
-}
-
-std::string Channel::getKey(void)
-{
-    return _key;
-}
-
-std::string Channel::getTopic(void)
-{
-    return _topic;
-}
-
-int Channel::getChannelLimit(void)
-{
-    return _limit;
-}
-
-void Channel::joinClient(Client* C)
-{
-    _clients.push_back(C);
-
-    for(size_t i = 0; i < _clients.size(); ++i)
-        _clients[i]->sending(RPL_JOIN(C->getPrefix(), _channelName));
-
-    this->setAdmin(C->isClosed());
-    this->nameReply(C);
-}
-
-void Channel::kickClient(Client* C, const std::string& reason, bool flagClosed)
-{
-    for(size_t i = 0; i < _clients.size(); ++i)
-        if (flagClosed)
-            _clients[i]->sending(RPL_KICK(_admin->getPrefix(), _channelName, C->getNICK(), reason));
-
-    std::vector<Client*>::iterator it = std::find(_clients.begin(), _clients.end(), C);
-    if (it != _clients.end())
-        _clients.erase(it);
-    it = std::find(_operators.begin(), _operators.end(), C);
-    if (it != _operators.end())
-        _operators.erase(it);
-    this->setAdmin(flagClosed);
-}
-
-bool Channel::isInChannel(Client* C)
-{
-    if (std::find(_clients.begin(), _clients.end(), C) == _clients.end())
-        return false;
-    return true;
-}
-
-Client *Channel::getClientByNick(std::string nickname)
-{
-    std::vector<Client*>::iterator it = _clients.begin();
-    for( ; it != _clients.end(); ++it)
-        if ((*it)->getNICK() == nickname)
-            return (*it);
-
-    return NULL;
-}
-
-bool Channel::isAdmin(Client* C)
-{
-    if (!isInChannel(C))
-        return false;
-    return C == _admin;
-}
-
-bool Channel::isOperator(Client *C)
-{
-    if (std::find(_operators.begin(), _operators.end(), C) == _operators.end())
-        return false;
-    return true;
-}
-
-bool Channel::isInviteOnly(void)
-{
-    return _inviteOnly;
-}
-
-bool Channel::topicModeIsOn(void)
-{
-    return _topicMode;
-}
-
-void Channel::sending(Client* C, const std::string& msg, const std::string& cmd)
-{
-    for (size_t i = 0; i < _clients.size(); ++i)
-        if (_clients[i] != C)
-            _clients[i]->sending(RPL_MSG(C->getPrefix(), cmd, _channelName, msg));
-}
-
-void Channel::sendingForBot(Client* C, const std::string& msg, const std::string& cmd)
-{
-    std::string mes = "@BOT ";
-    for (size_t i = 0; i < _clients.size(); ++i)
-        if (_clients[i] == C)
-            _clients[i]->sending(RPL_MSG(mes, cmd, _channelName, msg));
-}
-
-void Channel::nameReply(Client *C)
-{
-    // sending TOPIC to new user    
-    std::string topic = this->getTopic();
-    if (topic.empty())            
-        C->sending(RPL_NOTOPIC(_channelName + static_cast<char>(1)));
-    else            
-        C->sending(RPL_TOPIC(_channelName + static_cast<char>(1), topic));
-    
-    // sending channal's users list to new user
-    std::string nickList;
-    for (size_t i = 0; i < _clients.size(); ++i)
-    {
-        std::string prefix = (_clients[i] == _admin) ? "@" : "+";
-        nickList += prefix + _clients[i]->getNICK() + "  ";
-    }
-    C->sending(RPL_NAMREPLY(C->getNICK(), _channelName + static_cast<char>(1), nickList));
-    C->sending(RPL_ENDOFNAMES(C->getNICK(), _channelName + static_cast<char>(1)));
-}
+// ########################################################################################
 
 void Channel::setAdmin(bool flagClosed)
 {
@@ -179,13 +50,81 @@ void Channel::setAdmin(bool flagClosed)
             _clients[i]->sending(RPL_MSG(_admin->getPrefix(), "", _channelName, "is the new admin"));
 }
 
-void Channel::part(Client *C, std::string reason)
+// ########################################################################################
+
+void Channel::setInviteOnly(bool mode)
 {
-    if (std::find(_clients.begin(),_clients.end(),C) == _clients.end())
-        return ;
+    _inviteOnly = mode;
+}
+
+// ########################################################################################
+
+void Channel::setTopicMode(bool mode) {
+    _topicMode = mode;
+}
+
+// ########################################################################################
+
+void Channel::setChannelLimit(int limit) {
+    _limit = limit;
+}
+
+// ########################################################################################
+
+std::string Channel::getChannelName(void) {
+    return _channelName;
+}
+
+// ########################################################################################
+
+std::string Channel::getKey(void) {
+    return _key;
+}
+
+// ########################################################################################
+
+std::string Channel::getTopic(void) {
+    return _topic;
+}
+
+// ########################################################################################
+
+int Channel::getChannelLimit(void) {
+    return _limit;
+}
+
+// ########################################################################################
+
+Client* Channel::getClientByNick(std::string nickname)
+{
+    std::vector<Client*>::iterator it = _clients.begin();
+    for( ; it != _clients.end(); ++it)
+        if ((*it)->getNICK() == nickname)
+            return (*it);
+
+    return NULL;
+}
+
+// ########################################################################################
+
+void Channel::joinClient(Client* C)
+{
+    _clients.push_back(C);
+
     for(size_t i = 0; i < _clients.size(); ++i)
-        if (_clients[i] != C)
-            _clients[i]->sending(RPL_PART(C->getPrefix(), _channelName + reason));
+        _clients[i]->sending(RPL_JOIN(C->getPrefix(), _channelName));
+
+    this->setAdmin(C->isClosed());
+    this->nameReply(C);
+}
+
+// ########################################################################################
+
+void Channel::kickClient(Client* C, const std::string& reason, bool flagClosed)
+{
+    for(size_t i = 0; i < _clients.size(); ++i)
+        if (flagClosed)
+            _clients[i]->sending(RPL_KICK(_admin->getPrefix(), _channelName, C->getNICK(), reason));
 
     std::vector<Client*>::iterator it = std::find(_clients.begin(), _clients.end(), C);
     if (it != _clients.end())
@@ -193,15 +132,102 @@ void Channel::part(Client *C, std::string reason)
     it = std::find(_operators.begin(), _operators.end(), C);
     if (it != _operators.end())
         _operators.erase(it);
-    this->setAdmin(C->isClosed());
+    this->setAdmin(flagClosed);
 }
 
-bool Channel::emptyClients(void)
+// ########################################################################################
+
+bool Channel::isInChannel(Client* C)
 {
-    return _clients.empty();
+    if (std::find(_clients.begin(), _clients.end(), C) == _clients.end())
+        return false;
+    return true;
 }
 
-void Channel::replyWho(Client *C, int mode)
+// ########################################################################################
+
+bool Channel::isAdmin(Client* C)
+{
+    if (!isInChannel(C))
+        return false;
+    return C == _admin;
+}
+
+// ########################################################################################
+
+bool Channel::isOperator(Client* C)
+{
+    if (std::find(_operators.begin(), _operators.end(), C) == _operators.end())
+        return false;
+    return true;
+}
+
+// ########################################################################################
+
+bool Channel::isInviteOnly(void) {
+    return _inviteOnly;
+}
+
+// ########################################################################################
+
+bool Channel::topicModeIsOn(void) {
+    return _topicMode;
+}
+
+// ########################################################################################
+
+bool Channel::channelIsFull(void)
+{
+    if (_limit == 0)
+        return false;
+
+    return _limit <= static_cast<int>(_clients.size());
+}
+
+// ########################################################################################
+
+void Channel::sending(Client* C, const std::string& msg, const std::string& cmd)
+{
+    for (size_t i = 0; i < _clients.size(); ++i)
+        if (_clients[i] != C)
+            _clients[i]->sending(RPL_MSG(C->getPrefix(), cmd, _channelName, msg));
+}
+
+// ########################################################################################
+
+void Channel::sendingForBot(Client* C, const std::string& msg, const std::string& cmd)
+{
+    std::string mes = "@BOT ";
+    for (size_t i = 0; i < _clients.size(); ++i)
+        if (_clients[i] == C)
+            _clients[i]->sending(RPL_MSG(mes, cmd, _channelName, msg));
+}
+
+// ########################################################################################
+
+void Channel::nameReply(Client* C)
+{
+    // sending TOPIC to new user    
+    std::string topic = this->getTopic();
+    if (topic.empty())            
+        C->sending(RPL_NOTOPIC(_channelName + static_cast<char>(1)));
+    else            
+        C->sending(RPL_TOPIC(_channelName + static_cast<char>(1), topic));
+    
+    // sending channal's users list to new user
+    std::string nickList;
+    for (size_t i = 0; i < _clients.size(); ++i)
+    {
+        std::string prefix = (_clients[i] == _admin) ? "@" : "+";
+        nickList += prefix + _clients[i]->getNICK() + "  ";
+    }
+    C->sending(RPL_NAMREPLY(C->getNICK(), _channelName + static_cast<char>(1), nickList));
+    C->sending(RPL_ENDOFNAMES(C->getNICK(), _channelName + static_cast<char>(1)));
+}
+
+// ########################################################################################
+
+void Channel::replyWho(Client* C, int mode)
 {
     std::string replay;
     std::vector<std::string> atribut;
@@ -216,13 +242,43 @@ void Channel::replyWho(Client *C, int mode)
     C->sending(RPL_ENDOFWHO(C->getNICK(), _channelName));
 }
 
-void Channel::addOperator(Client *C)
+// ########################################################################################
+
+void Channel::part(Client* C, std::string reason)
+{
+    if (std::find(_clients.begin(),_clients.end(),C) == _clients.end())
+        return ;
+ 
+    for(size_t i = 0; i < _clients.size(); ++i)
+        if (_clients[i] != C)
+            _clients[i]->sending(RPL_PART(C->getPrefix(), _channelName + reason));
+
+    std::vector<Client*>::iterator it = std::find(_clients.begin(), _clients.end(), C);
+    if (it != _clients.end())
+        _clients.erase(it);
+    it = std::find(_operators.begin(), _operators.end(), C);
+    if (it != _operators.end())
+        _operators.erase(it);
+    this->setAdmin(C->isClosed());
+}
+
+// ########################################################################################
+
+bool Channel::emptyClients(void) {
+    return _clients.empty();
+}
+
+// ########################################################################################
+
+void Channel::addOperator(Client* C)
 {
     if (isInChannel(C) && !isOperator(C))
         _operators.push_back(C);
 }
 
-void Channel::removeOperator(Client *C)
+// ########################################################################################
+
+void Channel::removeOperator(Client* C)
 {
     if (isOperator(C))
     {
@@ -231,10 +287,4 @@ void Channel::removeOperator(Client *C)
     }
 }
 
-bool Channel::channelIsFull(void)
-{
-    if (_limit == 0)
-        return false;
-
-    return _limit <= static_cast<int>(_clients.size());
-}
+// ########################################################################################
